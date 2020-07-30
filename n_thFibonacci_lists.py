f44777faebfdbc714dad9442e8fabe276c2dab9b
Farhadul Islam <59443923+farhad324@users.noreply.github.com>
2020-07-30 12:26:41 +0600

    Update n_thFibonacci_lists.py

--- n_thFibonacci_lists.py
+++ n_thFibonacci_lists.py
@@ -1,12 +1,10 @@
 def nth_fibonacci(n):
     if (n <= 1):
         return n
-    fibo = []
-    fibo.append(0)
-    fibo.append(1)
+    fibo = [0,1]
     for i in range(n):
     	fibo.append(fibo[i]+fibo[i+1])
-
     return fibo[n]
 
 print(nth_fibonacci(int(input())))
+
